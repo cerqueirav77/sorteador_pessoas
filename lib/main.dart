1c70add2c78bcf435847053f4156277a734f122e
Victor Cerqueira <vcerqueira80.vc@gmail.com>
2025-03-14 19:34:48 -0300

    Código sorteador de pessoas

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> nomes = ['Guigui', 'Alcivan', 'Victor', 'Enzo Manoel', 'Andreas', 'Heitor'];
+  String nomeSorteado = '';
+
+  void _sortearNome() {
+    setState(() {
+      nomeSorteado = nomes[Random().nextInt(nomes.length)];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Nome sorteado:'),
+            Text(
+              nomeSorteado,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sortearNome,
+        tooltip: 'Sortear',
+        child: const Icon(Icons.shuffle),
+      ),
+    );
+  }
+}
